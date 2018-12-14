dfc3e107d7ebf458f7b9b0c2b87668a1728d49ee
gigantmysli <45815215+gigantmysli@users.noreply.github.com>
2018-12-14 08:47:52 +0100

    Add files via upload
    serial comms config

--- serialcomms.py
+++ serialcomms.py
@@ -1,22 +1,37 @@
 import serial
 import configparser
 from easygui import enterbox
-
-
+import os.path
 
 mConfig = configparser.ConfigParser()
+mSerialPortName = ""
 
-text = enterbox(msg = "WOT THE FOK",title="FOK FOK", default="FOK DIFOT")
-
-print(text)
-
-mConfig['serial'] = {'portName': 'COM1'}
+#find serial port name in config file or get it from user
+GetSPName()
 
 
-with open('commsConfig.ini', 'w') as configFile:
-  mConfig.write(configFile)
+print("Port Name: " + mSerialPortName)
+
+exit
 
 
-
-mSr = serial.Serial()
-mSr.port = "COM1"
+def GetSPName():
+  if not os.path.exists("commsConfig.ini"):
+    print("Config file not found")
+    mConfigFile = open("commsConfig.ini","w",encoding="utf8")
+    mSerialPortName = enterbox(msg = "Enter serial port name",title="Serial Interface Setup", default="COM1")
+    mConfig['serial'] = {'portName': mSerialPortName}
+    mConfig.write(mConfigFile)
+    mConfigFile.close()
+  else:
+    mConfig.read("commsConfig.ini")
+    mConfig.sections()
+    mSerialPortName = ""
+    if 'serial' in mConfig and 'portName' in mConfig['serial']:
+      mSerialPortName = mConfig['serial']['portName']
+    else:
+      mSerialPortName = enterbox(msg = "Enter serial port name",title="Serial Interface Setup", default="COM1")
+      mConfigFile = open("commsConfig.ini","w",encoding="utf8")
+      mConfig['serial'] = {'portName': mSerialPortName}
+      mConfig.write(mConfigFile)
+      mConfigFile.close()
